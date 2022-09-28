5cf210eaa42f00e3e95a27290a94bf8b33e5ec75
Afiq Hafizuddin <hafizuddinafiq@gmail.com>
2022-09-28 21:45:59 +0800

    download .NET console and config C# programming

---
+++ Program.cs
@@ -0,0 +1,15 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
+Console.WriteLine("Afiq Hafizuddin");
+
+
+Console.WriteLine("I'm learning programming in C#");
+// using System;
+
+// namespace 15 C#
+// {
+//     class Program
+//     {
+//         static void
+//     }
+// }
