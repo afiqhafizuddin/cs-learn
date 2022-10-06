df3b60afe1efab82a24d7c622f3193304fdac2e6
Afiq Hafizuddin <hafizuddinafiq@gmail.com>
2022-10-07 07:48:44 +0800

    add project and compiler dir

--- ConsoleApp/ConsoleApp/ConsoleApp/Program.cs
+++ ConsoleApp/ConsoleApp/ConsoleApp/Program.cs
@@ -1,2 +1,12 @@
 ﻿// See https://aka.ms/new-console-template for more information
 Console.WriteLine("Hello, World!");
+
+// Data Types
+
+int a = 99; // 	-2,147,483,648 to 2,147,483,647 ; 32-bit signed integer
+string exampleName = "Afiq Hafizuddin Bin Zainal";  // any character inside double quotes and can be anything
+byte exampleByte = 58; // 1 byte = 8 bits ; 0-255
+char exampleChar = 'a'; // 16-Bit unicode character ; U + 0000 to U + FFFF
+double exampleDouble = 14.45; // 64-bit double-precision floating point type 6
+
+Console.WriteLine(a);
