e35977c0322ac4f39f33183adfdbbe1f109cecdb
Afiq Hafizuddin <hafizuddinafiq@gmail.com>
2022-10-10 21:11:43 +0800

    add string concat, string interpolation examples

--- HelloWorld/HelloWorld/Program.cs
+++ HelloWorld/HelloWorld/Program.cs
@@ -88,12 +88,71 @@ Console.WriteLine(@"c:\source\repos");
 
 // Unicode escape characters
 
+// testing autosave feature on Visual Studio 2022
+
 Console.WriteLine("This is to test the autosave feature Visual Studio 2022");
 Console.WriteLine("Test again!");
 Console.WriteLine("Test again!");
 Console.WriteLine("Test again!");
 Console.WriteLine("Test again!");
 Console.WriteLine("Test again!");
+
+// String Concatenation
+
+string namaPertama = "Bob";
+string greeting = "Hello";
+
+string text = greeting + " " + namaPertama + "!";
+Console.WriteLine(text);
+
+// Using intermediate variable. Should be avoided unless there is solid reason 
+
+// avoid using intermediate variables by performing the concatenation operation as you need it.
+
+Console.WriteLine(greeting + " " + namaPertama + "!");
+
+// String Concatenation
+
+// String concatenation allows you to combine smaller literal and variable strings into a single string. 
+// Avoid creating intermediate variables if adding them doesn't increase readability
+
+
+// String Interpolation
+
+// String interpolation combines multiple values into a single literal string by using a "template"
+// and one or more interpolation expressions. An interpolation expression is a variable surrounded by an
+// opening and closing curly brace symbol { }.
+// The literal string becomes a template when it's prefixed by the $ character.
+
+string text1 = $"{greeting} {namaPertama}!";
+Console.WriteLine(text1);
+
+// combining verbatim literals and string concat
+
+string projectName = "Console-App";
+Console.WriteLine($@"C:\Output\{projectName}\Data");
+
+
+// String interpolation provides an improvement over string concatenation by reducing
+// the number of characters required in some situations.
+
+// You can combine string interpolation and verbatim literals by combining the
+// symbols for each and using that as a prefix for the string template.
+
+
+// Challenge
+
+string newProjectName = "ACME";
+string russianMessage = "\u041f\u043e\u0441\u043c\u043e\u0442\u0440\u0435\u0442\u044c \u0440\u0443\u0441\u0441\u043a\u0438\u0439 \u0432\u044b\u0432\u043e\u0434";
+
+Console.WriteLine($"View English Output:\n\tc:\\Exercise\\{newProjectName}\\datat.txt\n");
+Console.WriteLine($@"{russianMessage}:
+    c:\Execise\{newProjectName}\ru-RU\data.txt");
+
+
+
+
+
 
 
 
