3b8a239eb481aef0d18938b776f9ad0aee86d7f3
Afiq Hafizuddin <hafizuddinafiq@gmail.com>
2022-10-11 12:29:52 +0800

    add increment and decrement operators

--- MathOperation/MathOperation/Program.cs
+++ MathOperation/MathOperation/Program.cs
@@ -107,9 +107,30 @@ Console.WriteLine(value2);
 
 // Increment and decrement value
 
-int value = 0;
-value = value + 5;
-value += 5;
+int value = 1;
+
+value = value + 1;
+Console.WriteLine("First Increment: " + value);
+
+value += 1;
+Console.WriteLine("Second Increment: " + value);
+
+value++;
+Console.WriteLine("Third Increment: " + value);
+
+
+value = value - 1;
+Console.WriteLine("First Decrement: " + value);
+
+value -= 1;
+Console.WriteLine("Second Decrement: " + value);
+
+value--;
+Console.WriteLine("Third Decrement: " + value);
+
+// Positioning the increment 
+
+
 
 
 
