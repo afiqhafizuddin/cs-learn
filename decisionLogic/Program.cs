3cd8191b23583a628320f9ede34325225d6e9395
Afiq Hafizuddin <hafizuddinafiq@gmail.com>
2022-10-22 22:44:44 +0800

    add basic if statemnt to control logic

--- decisionLogic/Program.cs
+++ decisionLogic/Program.cs
@@ -1,1 +1,46 @@
-﻿Console.WriteLine("Learning and Using if-else statements in C#");
+﻿using System.Collections;
+using System.Reflection;
+
+Console.WriteLine("Learning and Using if-else statements in C#");
+
+Random dice = new Random();
+
+int roll1 = dice.Next(1, 7);
+int roll2 = dice.Next(1, 7);
+int roll3 = dice.Next(1, 7);
+
+int total = roll1 + roll2 + roll3;
+
+Console.WriteLine($"Dice Roll: {roll1} + {roll2} + {roll3} = {total}");
+
+if (total > 14)
+{
+    Console.WriteLine("You Win!");
+}
+
+if (total < 15)
+{
+    Console.WriteLine("Sorry, You Lose!");
+}
+
+// Boolean Expression
+
+string message = "The quick brown fox jumps over the lazy dog";
+bool result = message.Contains("dog"); // string.Contains() method to evaluate whether one string contains another string
+Console.WriteLine(result);
+
+if (message.Contains("fox"))
+{
+    Console.WriteLine("What does the fox say?");
+}
+
+// Simple Boolean expressions can be created using operators to compare two values
+
+// '==' : "equals" test for equality
+// '>'  : "greater than" test for value on the left is greater than value on the right
+// '<'  : "less than" test for value on the left is lesser than value on the right
+// '>=' : "greater than or equal"
+// '<=' : "lesser than or equal"
+
+
+
