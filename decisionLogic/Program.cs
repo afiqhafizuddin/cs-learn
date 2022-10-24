99b8b5a07262d9181922ecf6ba5e618442f40ae0
Afiq Hafizuddin <hafizuddinafiq@gmail.com>
2022-10-24 12:39:26 +0800

    use if, else and else if statements

--- decisionLogic/Program.cs
+++ decisionLogic/Program.cs
@@ -1,10 +1,13 @@
-﻿Console.WriteLine("Learning and Using if-else statements in C#");
+﻿Console.WriteLine("Learning and Using if-else statements in C#"); 
+
 
 Random dice = new Random();
 
 int roll1 = dice.Next(1, 7);
 int roll2 = dice.Next(1, 7);
 int roll3 = dice.Next(1, 7);
+
+
 
 int total = roll1 + roll2 + roll3;
 
@@ -13,25 +16,37 @@ Console.WriteLine($"Dice Roll: {roll1} + {roll2} + {roll3} = {total}");
 
 if ((roll1 == roll2) || (roll2 == roll3) || (roll1 == roll3))
 {
-    Console.WriteLine("You rolled doubles! +2 bonus to total");
-    total += 2;
+    if ((roll1 == roll2) && (roll2 == roll3))
+    {
+        Console.WriteLine("You rolled triples! +6 bonus to total");
+        total += 6;
+    }
+    else
+    {
+        Console.WriteLine("You rolled doubles! +2 bonus to total");
+        total +=2;
+    }
 }
 
-if ((roll1 == roll2) && (roll2 == roll3))
+if (total >= 16)
 {
-    Console.WriteLine("You rolled triples! +6 bonus to total");
-    total +=6;
+    Console.WriteLine("You Win a New Car!");
+}
+else if (total >= 10)
+{ 
+    Console.WriteLine("You Win a New Laptop!");
+
+}
+else if (total == 7)
+{
+    Console.WriteLine("You win a trip for two!");
+}
+else 
+{
+    Console.WriteLine("You win a kitten!");
 }
 
 
-if (total >= 15)
-{
-    Console.WriteLine("You Win!");
-}
-else
-{
-    Console.WriteLine("Sprry, You Lose!");
-}
 
 // Boolean Expression
 
@@ -53,6 +68,9 @@ if (message.Contains("fox"))
 // '<=' : "lesser than or equal"
 
 // Else and else-if statements
+
+
+
 
 
 
