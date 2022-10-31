4377f18262e0697872bf4c16f23bae8d6134f11d
Afiq Hafizuddin <hafizuddinafiq@gmail.com>
2022-11-01 03:39:42 +0800

    add using whitespaces practices

--- 6-csharpConventionPractices/Program.cs
+++ 6-csharpConventionPractices/Program.cs
@@ -30,4 +30,27 @@ foreach (var orderID in orderIDs)
     Console.WriteLine(orderID);
 }
 
+// Using whitespaces
+
+Random dice = new Random();
+
+int roll1 = dice.Next(1, 7);
+int roll2 = dice.Next(1, 7);
+int roll3 = dice.Next(1, 7);
+
+int total = roll1 + roll2 + roll3;
+Console.WriteLine($"Dice roll: {roll1} + {roll2} + {roll3} = {total}");
+
+if ((roll1 == roll2) || (roll2 == roll3) || (roll1 == roll3))
+{
+    if ((roll1 == roll2) && (roll2 == roll3))
+    {
+        Console.WriteLine("You rolled triples!");
+    }
+    else
+    {
+        Console.WriteLine("You rolled doubles!");
+    }
+}
+
 
