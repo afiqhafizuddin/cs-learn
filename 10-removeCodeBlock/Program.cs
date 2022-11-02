4c5c33b9a81499d5cad95a9dc366b4f6fc57fe39
Afiq Hafizuddin <hafizuddinafiq@gmail.com>
2022-11-02 15:58:35 +0800

    removing code blocks in else if and if statement

--- 10-removeCodeBlock/Program.cs
+++ 10-removeCodeBlock/Program.cs
@@ -3,3 +3,11 @@ if (flag)
     Console.WriteLine(flag);
 
 // else if and if statemnets without code blocks
+string name = "Robert";
+
+if (name == "Bob")
+    Console.WriteLine("Found Bob");
+else if (name == "Robert")
+    Console.WriteLine("Found Robert");
+else
+    Console.WriteLine("Found Chuck");
