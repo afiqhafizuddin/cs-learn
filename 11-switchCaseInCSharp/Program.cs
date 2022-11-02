9659d5b177dbe7503665b915fd404b4fdc3fbc93
Afiq Hafizuddin <hafizuddinafiq@gmail.com>
2022-11-02 22:15:26 +0800

    add switch-case statement

--- 11-switchCaseInCSharp/Program.cs
+++ 11-switchCaseInCSharp/Program.cs
@@ -1,2 +1,29 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Switch statement best used when
+// 1. have a single value you want to match against many possible values
+// 2. for any given match, you have to execute a couple lines of codes
+
+int employeeLevel = 200;
+string employeeName = "John Smith";
+
+string title = "";
+
+switch (employeeLevel)
+{
+    case 100:
+        title = "Junior Associate";
+        break;
+    case 200:
+        title = "Senior Associate";
+        break;
+    case 300:
+        title = "Manager";
+        break;
+    case 400:                       // to execute 
+        title = "Senior Manager";
+        break;
+    default:                        // when there is no match, .NET runtime will execute default value
+        title = "Associate";
+        break;
+}
+
+Console.WriteLine($"{employeeName}, {title}");
