72ab412be6c0b3e4f8af47e6b67d6d6d527378a7
Afiq Hafizuddin <hafizuddinafiq@gmail.com>
2022-11-05 00:08:16 +0800

    complete do-while challenge

--- 13-doWhile/Program.cs
+++ 13-doWhile/Program.cs
@@ -20,3 +20,34 @@ while (current >= 3)
 Console.WriteLine($"Last Number: {current}");
 */
 
+// Challenge
+
+// Initialization
+
+int heroHealth = 10;
+int monsterHealth = 10;
+
+Random attack = new Random(); 
+
+do
+{
+    // hero attacks monster
+    int attackVal = attack.Next(1, 11);  // all attacks valued from 1 to 10
+    monsterHealth -= attackVal; // new monsterHealth 
+    Console.WriteLine($"Monster was damaged and lost {attackVal} health and now has {monsterHealth} health.");
+
+    if (monsterHealth <= 0) continue; // continue to monster attack hero if monster still has health
+
+    // monster attack hero
+    attackVal = attack.Next(1, 11); // generate new random value
+    heroHealth -= attackVal;
+    Console.WriteLine($"Hero was damaged and lost {attackVal} health and now has {heroHealth} health.");
+
+} while (heroHealth > 0 && monsterHealth > 0);
+
+//Console.WriteLine( heroHealth > monsterHealth ? "Hero Wins!" : "Monster Wins!" );
+
+if (monsterHealth > heroHealth)
+    Console.WriteLine("Monster Wins!");
+else 
+    Console.WriteLine("Hero Wins!");
