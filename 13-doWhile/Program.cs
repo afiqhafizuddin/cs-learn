591b93bc6ddf021ac9c0daabefb387e771cb7882
Afiq Hafizuddin <hafizuddinafiq@gmail.com>
2022-11-04 23:47:58 +0800

    do-while loop and while loop in C#

--- 13-doWhile/Program.cs
+++ 13-doWhile/Program.cs
@@ -1,3 +1,22 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
-Console.WriteLine("Hello, World!");
+﻿Random random = new Random();   
+// int current = 0;
+int current = random.Next(1, 11);
+
+do
+{
+    current = random.Next(1, 11);
+    if (current >= 8) continue; // if current >= 8; skips the next line and redo the loop
+    Console.WriteLine(current);
+} while (current != 7); // stops the iteration if current = 7
+
+
+
+/*
+while (current >= 3)
+{
+    Console.WriteLine(current);
+    current = random.Next(1, 11);
+}
+Console.WriteLine($"Last Number: {current}");
+*/
+
