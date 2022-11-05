2d2c044cee6c49441bb0fc007850d87162e39653
Afiq Hafizuddin <hafizuddinafiq@gmail.com>
2022-11-05 21:26:22 +0800

    add signed integral value tuto

--- 14-dataType/Program.cs
+++ 14-dataType/Program.cs
@@ -1,2 +1,9 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// signed integral value
+
+Console.WriteLine("Signed integral types:");
+
+Console.WriteLine($"sbyte   : {sbyte.MinValue} to {sbyte.MaxValue}");
+Console.WriteLine($"short   : {short.MinValue} to {short.MaxValue}");
+Console.WriteLine($"int     : {int.MinValue} to {int.MinValue}");
+Console.WriteLine($"long    : {long.MinValue} to {long.MaxValue}");
+
