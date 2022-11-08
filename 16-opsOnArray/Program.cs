935b79388bcf1e1223e2011ea43fc146eabc90f3
Afiq Hafizuddin <hafizuddinafiq@gmail.com>
2022-11-08 23:30:30 +0800

    use Sort() and Reverse() of Array class to manipulate an array

--- 16-opsOnArray/Program.cs
+++ 16-opsOnArray/Program.cs
@@ -1,1 +1,19 @@
-﻿Console.WriteLine("Hello, World!");
+﻿// Sort() & Reverse()
+
+string[] pallets = { "B14", "A11", "B12", "A13" };
+
+Console.WriteLine("Sorted...");
+Array.Sort(pallets); // use Sort() method of the Array class to sort the items/elements in the array alphanumerically
+foreach (var pallet in pallets)
+{
+    Console.WriteLine($"-- {pallet}");
+}
+
+Console.WriteLine("");
+
+Console.WriteLine("Reversed...");
+Array.Reverse(pallets);  // use the Reverse() method of Array class to revers the items/elements in the array alphanumerically
+foreach (var pallet in pallets)
+{
+    Console.WriteLine($"-- {pallet}");
+}
