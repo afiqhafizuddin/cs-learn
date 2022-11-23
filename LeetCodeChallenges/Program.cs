304be3af365d1dd001a3ce21a7b0ea18a2848fee
Afiq Hafizuddin <hafizuddinafiq@gmail.com>
2022-11-24 00:34:59 +0800

    add sln to the LeetCode Problem #1

--- LeetCodeChallenges/Program.cs
+++ LeetCodeChallenges/Program.cs
@@ -1,3 +1,21 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿
 
+using System.Collections.Generic;
+
+public class Program
+{
+    public int[] TwoSum(int[] nums, int target)
+    {
+        var dic = new Dictionary<int, int>();
+
+        for (int i = 0; i < nums.Length; i++)
+        {
+            if (dic.ContainsKey(nums[i]))
+                return new int[] { dic[nums[i]], i };
+            else
+                dic[target - nums[i]] = i;
+        }
+
+        return new int[] { };
+    }
+}
